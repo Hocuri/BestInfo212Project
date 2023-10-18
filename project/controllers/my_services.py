7fc7f7f29bb83840e98d1aef73e7b9d708bb602e
Hocuri <hocuri@gmx.de>
2023-10-18 13:53:03 +0200

    Copy-paste code from letcure

---
+++ project/controllers/my_services.py
@@ -0,0 +1,40 @@
+from project import app
+from flask import render_template, request, redirect, url_for
+from project.models.my_dao import *
+
+@app.route('/get_cars', methods=['GET'])
+def query_records():
+    return findAllCars()
+
+# The method uses the registration number to find the car
+# object from database
+@app.route('/get_cars_by_reg_number', methods=['POST'])
+def find_car_by_reg_number():
+    record = json.loads(request.data)
+    print(record)
+    print(record['reg'])
+    return findCarByReg(record['reg'])
+
+@app.route('/save_car', methods=["POST"])
+def save_car_info():
+    record = json.loads(request.data)
+    print(record)
+    return save_car(record['make'], record['model'], record['reg'], record['year'], record['capacity'])
+
+# The method uses the registration number to find the car
+# object from database and updates other informaiton from
+# the information provided as input in the json object
+@app.route('/update_car', methods=['PUT'])
+def update_car_info():
+    record = json.loads(request.data)
+    print(record)
+    return update_car(record['make'], record['model'], record['reg'], record['year'], record['capacity'])
+
+# The method uses the registration number to find the car
+# object from database and removes the records
+@app.route('/delete_car', methods=['DELETE'])
+def delete_car_info():
+    record = json.loads(request.data)
+    print(record)
+    delete_car(record['reg'])
+    return findAllCars()
---
+++ project/models/my_dao.py
@@ -0,0 +1,86 @@
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import re
+
+URI = "neo4j+s://"
+AUTH = ("neo4j", "")
+
+def _get_connection() -> Driver:
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+    return driver
+
+def node_to_json(node):
+    node_properties = dict(node.items())
+    return node_properties
+
+def findAllCars():
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Car) RETURN a;")
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+def findCarByReg(reg):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Car) where a.reg=$reg RETURN a;", reg=reg)
+        print(cars)
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+def save_car(make, model, reg, year, capacity):
+    cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model, reg: $reg, \
+            year: $year, capacity:$capacity}) RETURN a;",
+            make = make, model = model, reg = reg, year = year, capacity = capacity)
+    nodes_json = [node_to_json(record["a"]) for record in cars]
+    print(nodes_json)
+    return nodes_json
+
+def update_car(make, model, reg, year, capacity):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Car{reg:$reg}) set a.make=$make, a.model=$model, a.year = $year, \
+                a.capacity = $capacity RETURN a;",
+                reg=reg, make=make, model=model, year=year, capacity=capacity)
+        print(cars)
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+def delete_car(reg):
+    _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg = reg)
+
+class Car:
+    def __init__(self, model, year, location, status): #constructer method, calles når du lager en ny instans av car
+        #self er en referanse til instansen av klassen som blir opprettet
+        self.model = model
+        self.year = year
+        self.location = location
+        self.status = status
+
+#get metode, hent ut modellen
+#retrieve the value of the model attribute
+    def get_Model(self):
+        return self.model
+
+#set metode, sett modellen
+#allows you to set or update the model attribute 
+    def set_Model(self, value):
+        self.model = value
+
+    def get_Year(self):
+        return self.year
+    
+    def set_Year(self, value):
+        self.year = value
+
+    def get_Location(self):
+        return self.location
+    
+    def set_Location(self, value):
+        self.location = value
+
+    def get_Status(self):
+        return self.status
+    
+    def set_Status(self, value):
+        self.status = value
