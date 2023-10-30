d92d048b0f68485cd1539db4eb13e6d3604e5274
FredViken <91135807+FredViken@users.noreply.github.com>
2023-10-30 12:28:04 +0100

    added db connection

--- project/models/my_dao.py
+++ project/models/my_dao.py
@@ -1,8 +1,8 @@
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import re
 
-URI = "neo4j+s://"
-AUTH = ("neo4j", "")
+URI = "neo4j+s://8b2aea2e.databases.neo4j.io"
+AUTH = ("neo4j", "kkj4obvCz4EyhJG6XVHP2oMORQWy-vBDfon6LfPViLc")
 
 def _get_connection() -> Driver:
     driver = GraphDatabase.driver(URI, auth=AUTH)
@@ -16,35 +16,39 @@ def node_to_json(node):
 def findAllCars():
     with _get_connection().session() as session:
         cars = session.run("MATCH (a:Car) RETURN a;")
-        nodes_json = [node_to_json(record["a"]) for record in cars]
-        print(nodes_json)
-        return nodes_json
+        # nodes_json = [node_to_json(record["a"]) for record in cars]
+        # print(nodes_json)
+        return 'Ok'
 
 def findCarByReg(reg):
     with _get_connection().session() as session:
         cars = session.run("MATCH (a:Car) where a.reg=$reg RETURN a;", reg=reg)
-        print(cars)
-        nodes_json = [node_to_json(record["a"]) for record in cars]
-        print(nodes_json)
-        return nodes_json
+        # print(cars)
+        # nodes_json = [node_to_json(record["a"]) for record in cars]
+        # print(nodes_json)
+        return 'Ok'
 
 def save_car(make, model, reg, year, capacity):
     cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model, reg: $reg, \
             year: $year, capacity:$capacity}) RETURN a;",
             make = make, model = model, reg = reg, year = year, capacity = capacity)
-    nodes_json = [node_to_json(record["a"]) for record in cars]
-    print(nodes_json)
-    return nodes_json
+    
+    print(cars.records)
+    # nodes_json = [node_to_json(record["a"]) for record in cars]
+    # print(nodes_json)
+    # return nodes_json
+    return 'Ok'
 
 def update_car(make, model, reg, year, capacity):
     with _get_connection().session() as session:
         cars = session.run("MATCH (a:Car{reg:$reg}) set a.make=$make, a.model=$model, a.year = $year, \
                 a.capacity = $capacity RETURN a;",
                 reg=reg, make=make, model=model, year=year, capacity=capacity)
-        print(cars)
-        nodes_json = [node_to_json(record["a"]) for record in cars]
-        print(nodes_json)
-        return nodes_json
+        # print(cars)
+        # nodes_json = [node_to_json(record["a"]) for record in cars]
+        # print(nodes_json)
+
+        return 'Ok'
 
 def delete_car(reg):
     _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg = reg)
