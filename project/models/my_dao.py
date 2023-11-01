76e2ee765a8b145dae83363335cd912123c2b9c0
Hocuri <hocuri@gmx.de>
2023-11-01 13:00:56 +0100

    Remove Car class

--- project/models/my_dao.py
+++ project/models/my_dao.py
@@ -51,49 +51,6 @@ def delete_car(reg):
     _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg = reg)
     return jsonify('Car deleted'), 200
 
-class Car:
-    def __init__(self, model, year, location, status, reg): #constructer method, calles når du lager en ny instans av car
-        #self er en referanse til instansen av klassen som blir opprettet
-        self.model = model
-        self.year = year
-        self.location = location
-        self.status = status
-        self.reg = reg
-
-#get metode, hent ut modellen
-#retrieve the value of the model attribute
-    def get_Model(self):
-        return self.model
-
-#set metode, sett modellen
-#allows you to set or update the model attribute 
-    def set_Model(self, value):
-        self.model = value
-
-    def get_Year(self):
-        return self.year
-    
-    def set_Year(self, value):
-        self.year = value
-
-    def get_Location(self):
-        return self.location
-    
-    def set_Location(self, value):
-        self.location = value
-
-    def get_Status(self):
-        return self.status
-    
-    def set_Status(self, value):
-        self.status = value
- 
-    def get_Reg(self):
-        return self.reg
-    
-    def set_Reg(self, value):
-        self.reg = value
-
 
 #Customer
 # Saves customer, returns 201 status
